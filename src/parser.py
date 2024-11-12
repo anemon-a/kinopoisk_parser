d2bf9c73cd5e921d603cb5ed439356e833e81e6a
Anna Matskevich <annamatskevich866@gmail.com>
2024-11-12 18:39:23 +0700

    fix: delete unused imports

--- src/parser.py
+++ src/parser.py
@@ -1,10 +1,5 @@
 from dataclasses import dataclass, asdict
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup, PageElement
 from time import sleep
 import logging
@@ -35,8 +30,8 @@ class Movie:
     ):
 
         year = year.text.split(",")
-        country = country_and_director.text.split(' • ')[0]
-        director = country_and_director.text.split('Режиссёр: ')[1]
+        country = country_and_director.text.split(" • ")[0]
+        director = country_and_director.text.split("Режиссёр: ")[1]
 
         self.name = name.text
         self.year = int(year[1][1:]) if not year[0] else int(year[0])
@@ -51,21 +46,16 @@ def get_movie_information(element: PageElement) -> Movie:
         "span",
         class_="styles_mainTitle__IFQyZ styles_activeMovieTittle__kJdJj",
     )
-    year = element.find(
-        "span", class_="desktop-list-main-info_secondaryText__M_aus"
-    )
+    year = element.find("span", class_="desktop-list-main-info_secondaryText__M_aus")
     country_and_director = element.find(
         "span", class_="desktop-list-main-info_truncatedText__IMQRP"
     )
-    rating = element.find(
-        "span", class_=re.compile("styles_kinopoiskValue__nkZEC")
-    )
+    rating = element.find("span", class_=re.compile("styles_kinopoiskValue__nkZEC"))
     is_on_kinopoisk = element.find(
         "element", class_="styles_onlineButton__ER9Vt styles_inlineItem___co22"
     )
 
-    movie = Movie(name, year, country_and_director,
-                  rating, is_on_kinopoisk)
+    movie = Movie(name, year, country_and_director, rating, is_on_kinopoisk)
     return movie
 
 
@@ -77,12 +67,11 @@ def parse_kinopoisk() -> None:
     try:
         for page in range(1, 21):
             driver.get(url + f"?page={page}")
-            sleep(3)
+            sleep(1)
             soup = BeautifulSoup(driver.page_source, "lxml")
             element = soup.find_all("div", attrs={"data-tid": "679d3e26"})
             if not element:
-                raise RuntimeError(
-                    f"Can't find necessary elements on page {page}")
+                raise RuntimeError(f"Can't find necessary elements on page {page}")
 
             for e in element:
                 data.append(asdict(get_movie_information(element=e)))
@@ -97,7 +86,8 @@ def parse_kinopoisk() -> None:
             json.dump(data, f, indent=2, ensure_ascii=False)
             if len(data) != 1000:
                 logger.info(
-                    "instead of 1000 movies, %s were recorded in the file", len(data))
+                    "instead of 1000 movies, %s were recorded in the file", len(data)
+                )
             logger.info("Writed to file %s", "movies.json")
 
 
